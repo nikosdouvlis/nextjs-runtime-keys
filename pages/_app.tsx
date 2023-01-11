aba81306a56bc379610945c2e4675a8c14426b30
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-01-11 20:30:27 +0200

    init setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {ClerkProvider, SignInButton, UserButton} from '@clerk/nextjs'
+import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ClerkProvider {...pageProps} >
+    <UserButton/>
+    <SignInButton/>
+    <Component {...pageProps} />
+  </ClerkProvider>
 }
---
+++ pages/user/[[...index]].tsx
@@ -0,0 +1,28 @@
+import { UserProfile } from '@clerk/nextjs';
+import type { GetServerSideProps, NextPage } from 'next';
+import React from 'react';
+import { getAuth } from '@clerk/nextjs/server';
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const auth = getAuth(context.req);
+  console.log('getServerSideProps', auth);
+  console.log('getToken', await auth.getToken({ template: 'hasura1' }));
+  return { props: {} };
+};
+
+const UserProfilePage: NextPage = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <UserProfile />
+    </div>
+  );
+};
+
+export default UserProfilePage;
+
+// drivepoint.gr/user
+//   -> middleware -> clerk auth flow -> middleware handler -> inject headers
+//         -> /user page ->
+//               -> getServerSideProps
+//                            -> getAuth
+//                                       -> render page
